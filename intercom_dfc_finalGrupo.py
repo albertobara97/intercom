2ab84e306c18bb00550aa93962edb8e612494697
root <root@DESKTOP-4KASGLC.localdomain>
2019-11-26 10:24:02 +0100

    issue medio resuelto

--- intercom_dfc_finalGrupo.py
+++ intercom_dfc_finalGrupo.py
@@ -10,17 +10,24 @@ class Intercom_dfc(Intercom_binaural):
 
     def init(self, args):
         Intercom_binaural.init(self, args)
+        self.saved = [0]*self.cells_in_buffer #64 de tamaño
+        self.chunkmemory = [0]*self.cells_in_buffer
+        self.min = 32 #numero de bitplanes a enviar
 
     def record_send_and_play_stereo(self, indata, outdata, frames, time, status):
-        print("rcp en dfc")
+        print("rsp en dfc")
         indata[:,0] -= indata[:,1]
         self.record_and_send(indata)
         self._buffer[self.played_chunk_number % self.cells_in_buffer][:,0] += self._buffer[self.played_chunk_number % self.cells_in_buffer][:,1]
         self.play(outdata)
 
     def record_and_send(self, indata):
-        
+        print("played chunk", self.played_chunk_number)
+        #self.saved[self.played_chunk_number] = self.played_chunk_number
+        #print("Lista", *self.saved)
         for bitplane_number in range(self.number_of_channels*16-1, -1, -1):
+            #self.saved[self.played_chunk_number] = bitplane_number
+            #print("Lista", *self.saved)
             #cabecera = [self.recorded_chunk_number][bitplane_number]
             #: devuelve todo el contenido de ese lado del array. >> desplaza los bits recogidos de ese array tantas casillas como avanza la i 
             # en el bucle local. & 1 hace que cuando un bit es negativo, el desplazamiento no arrastre mas 1 creados por la propiedad de desplazar los bits de un numero negativo
@@ -36,6 +43,7 @@ class Intercom_dfc(Intercom_binaural):
         #Cuando se han enviado todos los planos de bits, esta variable aumenta en 1 para hacer un seguimiento del contador de chunks enviados.
         self.recorded_chunk_number = (self.recorded_chunk_number + 1) % self.MAX_CHUNK_NUMBER
 
+
     def receive_and_buffer(self):
         message, source_address = self.receiving_sock.recvfrom(Intercom_binaural.MAX_MESSAGE_SIZE)
         #Despieza el paquete recibido en varias variables
@@ -50,6 +58,13 @@ class Intercom_dfc(Intercom_binaural):
         #necesario porque el buffer almacena los datos en un array bidimensional, si asignamos sin el operador el array que nos llega
         # del bitplane, sobreescribiriamos los valores de las columnas que no nos interesan tomar del array
         self._buffer[chunk_number % self.cells_in_buffer][:, bitplane_number%self.number_of_channels] |= (bitplane << bitplane_number//self.number_of_channels)
+        if(self.chunkmemory[chunk_number%self.cells_in_buffer] < chunk_number):
+            self.saved[chunk_number%self.cells_in_buffer] = 0
+            print("\n\n\n\nHE CAMBIADO ", (chunk_number%self.cells_in_buffer))
+
+        self.chunkmemory[chunk_number%self.cells_in_buffer] = chunk_number
+        self.saved[chunk_number%self.cells_in_buffer] += 1
+        print("Lista", *self.saved)
         return chunk_number
 
 if __name__ == "__main__":
